ab02cfc6fbd5bb0d15687dbf0e84424744536855
gumelarid <galuhprahadi96@gmail.com>
2021-12-12 23:31:18 +0700

    update

---
+++ assets/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed", {
+  // Waits 1000ms after typing "First"
+  strings: ["Web Developer", "Content Writer", "and I build things for the web"],
+  typeSpeed: 100,
+  delaySpeed: 150,
+  loop: true,
+});
